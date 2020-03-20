8fd5154cc1c2fc3c1765333b4909bda4042aa54f
maheswarii <62419626+maheswarii@users.noreply.github.com>
2020-03-20 17:15:44 +0700

    first commit

---
+++ Assets/mantul.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mantul : MonoBehaviour
+{
+    public int speed=20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        GetComponent<Rigidbody2D>().velocity =  new Vector2(1,-1)*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name=="dindingKanan" || other.collider.name=="dindingKiri"){
+            GetComponent<Transform>().position = new Vector2(0,0);
+        }
+    }
+}
